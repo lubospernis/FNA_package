3ae642f28239b7548f0d1bb1edce19ba133c5035
Lubos Pernis <lubospernis@Luboss-MacBook-Pro.local>
2020-04-03 13:02:22 +0200

    new_command

--- R/command_output.R
+++ R/command_output.R
@@ -6,12 +6,11 @@
 #' @export
 #'
 #' @examples
-command_output <- function() {
+command_output <- function(id = NULL) {
   login_data <- options('FNA')$FNA
   workspace <- options('FNA_workspace')$FNA_workspace
   url <- paste0(login_data$url, 'rest/resources/', login_data$username, '/', workspace, '/context/output')
   t <- httr::GET(url, httr::add_headers(Authorization = paste("Bearer", login_data$token, sep = " ")),
                  query = list(id = id))
-  print(t)
   return(httr::content(t))
 }
