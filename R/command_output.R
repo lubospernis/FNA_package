ad11d6695ddc63dd43c6496928e9cbfdcd5fd533
Lubos Pernis <lubospernis@Luboss-MacBook-Pro.local>
2020-04-03 12:54:42 +0200

    new function added

---
+++ R/command_output.R
@@ -0,0 +1,17 @@
+#' Get code status - BETA (id parameter not implemented from the documentation)
+#'
+#' @param id
+#'
+#' @return
+#' @export
+#'
+#' @examples
+command_output <- function() {
+  login_data <- options('FNA')$FNA
+  workspace <- options('FNA_workspace')$FNA_workspace
+  url <- paste0(login_data$url, 'rest/resources/', login_data$username, '/', workspace, '/context/output')
+  t <- httr::GET(url, httr::add_headers(Authorization = paste("Bearer", login_data$token, sep = " ")),
+                 query = list(id = id))
+  print(t)
+  return(httr::content(t))
+}
